11795f3b39b070891554c3e0df31bd1cfe8e5846
Eduard Avetisyan <eduardavetisyan@gmail.com>
2017-10-09 20:23:22 +0400

    added functions

--- app.js
+++ app.js
@@ -28,44 +28,14 @@ const readBody = (request, cb) => {
   })
 }
 
-http.createServer((request, response) => {
-  const { headers, method, url } = request
-  // /api/tweets POST
-  if (url === '/api/tweets' && method === 'POST'){
-    readBody(request, (body) => {
-      let bodyJSON = JSON.parse(body)
-      bodyJSON.forEach((tweet) => {
-        tweet.id = Math.floor(Math.random() * 999999999)
-      })
-
-      read('tweets.json', (data) => {
-        if(!data.toString()){ //file empty
-          let tweets = {}
-          tweets.tweets = bodyJSON
-          write('tweets.json', tweets)
-        }
-        else{ //file has content
-          let currentData = JSON.parse(data.toString())
-          currentData.tweets = currentData.tweets.concat(bodyJSON)
-          write('tweets.json', currentData)
-        }
-
-        response.statusCode = 200
-        response.end(`received:\n${body}`)
-      })
-    })
-  }
-  // /api/tweets GET
-  else if (url === '/api/tweets' && method === 'GET'){
-    read('tweets.json', (data) => {
+const getTweet = (request, response, type) => {
+  read('tweets.json', (data) => {
+    if (type === 'ALL'){
       response.writeHead(200, {"Content-Type": "application/json"});
       response.end(data.toString())
-    })
-  }
-  // /api/tweets/{id} GET
-  else if (url.startsWith('/api/tweets/') && method === 'GET'){
-    const id = url.split('/')[3]
-    read('tweets.json', (data) => {
+    }
+    else if (type === 'SINGLE'){
+      const id = request.url.split('/')[3]
       let found = false
       if(data.toString()){
         let currentData = JSON.parse(data.toString())
@@ -78,17 +48,19 @@ http.createServer((request, response) => {
         })
 
         if(!found){
+          response.statusCode = 404
           response.end(`tweet with id ${id} not found!`)
         }
       }
-    })
-  }
-  // /api/tweets/{id} DELETE
-  else if (url.startsWith('/api/tweets/') && method === 'DELETE'){
-    const id = url.split('/')[3]
+    }
+  })
+}
+
+const modifyTweet = (request, response, type) => {
+  readBody(request, (bodyData) => {
+    const id = request.url.split('/')[3]
     read('tweets.json', (data) => {
       let found = false
-      let deletedTweet
       let newData = []
       if(data.toString()){
         let currentData = JSON.parse(data.toString())
@@ -97,7 +69,11 @@ http.createServer((request, response) => {
             newData.push(tweet)
           } else {
             found = true
-            deletedTweet = tweet
+            if (type === 'EDIT'){
+              let newContent = JSON.parse(bodyData)[0]
+              newContent.id = tweet.id
+              newData.push(newContent)
+            }
           }
         })
 
@@ -105,83 +81,96 @@ http.createServer((request, response) => {
           currentData.tweets = newData
           write('tweets.json', currentData)
           response.writeHead(200, {"Content-Type": "application/json"});
-          response.end(`{"message": "Successfully deleted tweet ${deletedTweet.id}"}`)
+          response.end(`{"message": "Successfully ${type === 'DELETE' ? 'deleted' : 'updated'} tweet ${id}"}`)
         }
       }
-
       response.end(`tweet with id ${id} not found!`)
     })
-  }
-  // /api/tweets/{id} PUT
-  else if (url.startsWith('/api/tweets/') && method === 'PUT'){
-    const id = url.split('/')[3]
-    readBody(request, (bodyData) => {
-      console.log(new Date().valueOf())
-      read('tweets.json', (data) => {
-        let found = false
-        let updatedTweet
-        let newData = []
-        if(data.toString()){
-          let currentData = JSON.parse(data.toString())
-          currentData.tweets.forEach((tweet) => {
-            if (tweet.id != id){
-              newData.push(tweet)
-            } else {
-              found = true
-              updatedTweet = tweet
-              let newContent = JSON.parse(bodyData)[0]
-              newContent.id = Math.floor(Math.random() * 999999999)
-              newData.push(newContent)
+  })
+}
 
-            }
-          })
+const addTweets = (request, response) => {
+  readBody(request, (body) => {
+    let bodyJSON = JSON.parse(body)
+    bodyJSON.forEach((tweet) => {
+      tweet.id = Math.floor(Math.random() * 999999999)
+    })
+    read('tweets.json', (data) => {
+      if(!data.toString()){ //file empty
+        let tweets = {}
+        tweets.tweets = bodyJSON
+        write('tweets.json', tweets)
+      }
+      else{ //file has content
+        let currentData = JSON.parse(data.toString())
+        currentData.tweets = currentData.tweets.concat(bodyJSON)
+        write('tweets.json', currentData)
+      }
 
-          if(found){
-            currentData.tweets = newData
-            write('tweets.json', currentData)
-            response.writeHead(200, {"Content-Type": "application/json"});
-            response.end(`{"message": "Successfully updated tweet ${updatedTweet.id}"}`)
-          }
-        }
+      response.statusCode = 200
+      response.end(`received:\n${body}`)
+    })
+  })
+}
 
-        response.end(`tweet with id ${id} not found!`)
+const showAllTweets = (request, response) => {
+  let found = false
+  let buildHTML = '<html><body><ul>'
+  read('tweets.json', (data) => {
+    if (data.toString()){
+      found = true
+      JSON.parse(data).tweets.forEach((tweet) => {
+        buildHTML += `<li>"${tweet.tweet}" by <b>${tweet.user}</b></li>`
       })
-    })
+    }
+    if(!found) buildHTML += 'NO TWEETS AVAILABLE'
+    buildHTML += '</ul></body></html>'
+    response.end(buildHTML)
+  })
+}
 
+const showOneTweet = (request, response) => {
+  const id = request.url.split('/')[1]
+  let found = false
+  let buildHTML = '<html><body><p>'
+  read('tweets.json', (data) => {
+    if (data.toString()){
+      JSON.parse(data).tweets.forEach((tweet) => {
+        if (tweet.id == id){
+          found = true
+          buildHTML += `"${tweet.tweet}" by <b>${tweet.user}</b>`
+        }
+      })
+    }
+    if (!found){buildHTML += 'TWEET NOT FOUND'}
+    buildHTML += '</p></body></html>'
+    response.end(buildHTML)
+  })
+}
+
+http.createServer((request, response) => {
+  const { headers, method, url } = request
+
+  if (url === '/api/tweets' && method === 'POST'){
+    addTweets(request, response)
   }
-  // GET / - in html <ul> <li> list all the tweets
+  else if (url === '/api/tweets' && method === 'GET'){
+    getTweet(request, response, 'ALL')
+  }
+  else if (url.startsWith('/api/tweets/') && method === 'GET'){
+    getTweet(request, response, 'SINGLE')
+  }
+  else if (url.startsWith('/api/tweets/') && method === 'DELETE'){
+    modifyTweet(request, response, 'DELETE')
+  }
+  else if (url.startsWith('/api/tweets/') && method === 'PUT'){
+    modifyTweet(request, response, 'EDIT')
+  }
   else if (url === '/' && method === 'GET'){
-    let found = false
-    let buildHTML = '<html><body><ul>'
-    read('tweets.json', (data) => {
-      if (data.toString()){
-        found = true
-        JSON.parse(data).tweets.forEach((tweet) => {
-          buildHTML += `<li>"${tweet.tweet}" by <b>${tweet.user}</b></li>`
-        })
-      }
-      if(!found) buildHTML += 'NO TWEETS AVAILABLE'
-      buildHTML += '</ul></body></html>'
-      response.end(buildHTML)
-    })
-  //  GET /id - in html <p> show single tweet
-  } else if(method === 'GET'){
-    const id = url.split('/')[1]
-    let found = false
-    let buildHTML = '<html><body><p>'
-    read('tweets.json', (data) => {
-      if (data.toString()){
-        JSON.parse(data).tweets.forEach((tweet) => {
-          if (tweet.id == id){
-            found = true
-            buildHTML += `"${tweet.tweet}" by <b>${tweet.user}</b>`
-          }
-        })
-      }
-      if (!found){buildHTML += 'TWEET NOT FOUND'}
-      buildHTML += '</p></body></html>'
-      response.end(buildHTML)
-    })
+    showAllTweets(request, response)
+  }
+  else if(method === 'GET'){
+    showOneTweet(request, response)
   }
   else {
     response.statusCode = 400
